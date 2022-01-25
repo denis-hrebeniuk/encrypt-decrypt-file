528fe77ad7fb8f3a60d1c7754fda8eeebe33801b
Denis Hrebeniuk <denis.hrebeniuk2006@gmail.com>
2022-01-25 18:29:03 +0200

    Release version 1.0.0

---
+++ readme.md
@@ -0,0 +1,28 @@
+# Encryption/Decryption file
+
+## About
+
+This project is a simple file encryptor/decryptor.
+
+## Installation
+
+- Install [**Node.js**](https://nodejs.org/) 15.14.0 or newer
+- Download repository or clone
+- Install npm dependencies
+```
+npm install
+```
+
+## Usage
+
+To encrypt file you just need to specify file, encryption password, output file. Use `node . --help` to get all commands.
+
+> Encryption example
+```
+node . --encrypt-file "example.txt" --encrypt-secret-key readme --encrypt-output-file "output.txt" --encrypt
+```
+
+> Decryption example
+```
+node . --encrypt-file "example.txt" --encrypt-secret-key readme --encrypt-output-file "output.txt"
+```
---
+++ src/index.js
@@ -0,0 +1,69 @@
+const { program } = require("commander"); // Import commander package for building cli
+const fs = require("fs"); // File System to work with file
+const cryptoJs = require("crypto-js"); // JavaScript library of crypto standards.
+
+program
+  .requiredOption("--encrypt-file <file>", "Path to file") // Path to file
+  .requiredOption("--encrypt-secret-key <secret key>", "Encrypt secret key")
+  .requiredOption("--encrypt-output-file <file>", "Path to output file") // Path to ouput file, that we will encrypt
+  .option("--encrypt", "Encrypt file or decrypt") // Encrypt file or decrypt
+  .parse();
+
+const options = program.opts(); // Get cli options
+
+if (Object.keys(options).length >= 3) {
+  if (options.encrypt) {
+    const pathFileToEncrypt = options.encryptFile;
+    const encryptionSecretKey = options.encryptSecretKey;
+    const pathOutputFile = options.encryptOutputFile;
+
+    fs.readFile(pathFileToEncrypt, (error, data) => {
+      if (error) {
+        return console.log(`[Encrypt File] ${error}`);
+      }
+
+      let encryptedData = cryptoJs.AES.encrypt(
+        data.toString(),
+        encryptionSecretKey
+      ); // Encrypt data of file to AES
+
+      fs.writeFile(pathOutputFile, encryptedData.toString(), (error) => {
+        if (error) {
+          return console.log(`[Encrypt File] ${error}`);
+        }
+
+        console.log(
+          `[Encrypt File] Successfully wrote! Output: ${pathOutputFile}`
+        );
+      });
+    });
+  } else {
+    const pathFileToDecrypt = options.encryptFile;
+    const encryptionSecretKey = options.encryptSecretKey;
+    const pathOutputFile = options.encryptOutputFile;
+
+    fs.readFile(pathFileToDecrypt, (error, data) => {
+      if (error) {
+        return console.log(`[Decrypt File] ${error}`);
+      }
+
+      let decryptedData = cryptoJs.AES.decrypt(
+        data.toString(),
+        encryptionSecretKey
+      ); // Decrypt data of file
+      decryptedData = decryptedData.toString(cryptoJs.enc.Utf8);
+
+      fs.writeFile(pathOutputFile, decryptedData.toString(), (error) => {
+        if (error) {
+          return console.log(`[Decrypt File] ${error}`);
+        }
+
+        console.log(
+          `[Decrypt File] Successfully wrote! Output: ${pathOutputFile}`
+        );
+      });
+    });
+  }
+} else {
+  return program.help();
+}
